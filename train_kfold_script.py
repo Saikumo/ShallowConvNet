479bb26f5215f43e35d32f998179369149bf0d34
Saikumo <saikumo11@gmail.com>
2025-12-19 15:39:09 +0800

    [Update] support multi gpu

--- train_kfold_script.py
+++ train_kfold_script.py
@@ -4,7 +4,7 @@ from train_kfold import train_kfold
 
 def train_kfold_script(local_rank):
     if (local_rank == 0):
-        subjects = [1, 3, 5, 7, 9]sc
+        subjects = [1, 3, 5, 7, 9]
         device = 'cuda:0'
     else:
         subjects = [2, 4, 6, 8]
