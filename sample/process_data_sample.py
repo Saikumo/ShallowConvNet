7a934245fa5888d5cab0fb3294fee1bed6d76491
Saikumo <saikumo11@gmail.com>
2025-12-29 12:51:56 +0800

    [Update] update load dataset

--- sample/process_data_sample.py
+++ sample/process_data_sample.py
@@ -1,8 +1,8 @@
 from preprocess_data import load_bnci2014_001_data_from_moabb
 
 
-def preprocess_bnci2014_001_sample():
-    X, y = load_bnci2014_001_data_from_moabb(subject_id=1, train=True)
+def preprocess_bnci2014_001_sample(tmin=-0.5, tmax=4):
+    X, y = load_bnci2014_001_data_from_moabb(subject_id=1, train=True, tmin=tmin, tmax=tmax)
 
     mean = X.mean(dim=(0, 2), keepdim=True)
     std = X.std(dim=(0, 2), keepdim=True)
