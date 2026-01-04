bcdbea4ba6de16b6d748085893383b9454f890b3
Saikumo <saikumo11@gmail.com>
2026-01-04 17:09:50 +0800

    [Create] test speedup train

---
+++ kfold/train_kfold.py
@@ -0,0 +1,99 @@
+from eeg_dataset import EEGDataset
+from preprocess.preprocess_data import *
+from shallow_convnet_speedup import ShallowConvNetSpeedup
+from train_one_epoch import *
+import numpy as np
+from torch.utils.data import DataLoader
+
+
+def train_kfold(device, subjectId=1, patience=20, epochs=200, batch_size=64):
+    folds = preprocess_kfold_bnci2014_001(subject_id=subjectId, n_splits=5, random_state=42)
+
+    print(f"device {device},subject {subjectId}")
+
+    best_epochs = []
+    best_losses = []
+    best_loss_accs = []
+    best_losses_kappas = []
+
+    for i, fold in enumerate(folds):
+        X_train = fold['X_train']
+        y_train = fold['y_train']
+        X_val = fold['X_val']
+        y_val = fold['y_val']
+        train_dataset = EEGDataset(X_train, y_train)
+        val_dataset = EEGDataset(X_val, y_val)
+        train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True,
+                                  pin_memory=(device.type == 'cuda'),
+                                  worker_init_fn=lambda _: np.random.seed(common.random_seed),
+                                  num_workers=0)
+        val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False,
+                                pin_memory=(device.type == 'cuda'),
+                                worker_init_fn=lambda _: np.random.seed(common.random_seed),
+                                num_workers=0)
+
+        model = ShallowConvNetSpeedup(X_train.shape)
+        model.to(device)
+        criterion = torch.nn.CrossEntropyLoss()
+        optimizer = torch.optim.Adam(model.parameters(), lr=1e-3, eps=1e-8)
+
+        best_loss = float("inf")
+        best_epoch = 0
+        best_loss_acc = float("inf")
+        counter = 0
+        best_loss_kappa = float("inf")
+
+        for epoch in range(epochs):
+            train_loss, train_acc, train_kappa = train_one_epoch(model, train_loader, optimizer, criterion, device)
+            val_loss, val_acc, val_kappa = eval_one_epoch(model, val_loader, criterion, device)
+
+            # print(f"Fold {i + 1}, Epoch {epoch + 1}/{epochs} | "
+            #       f"Train Loss: {train_loss:.4f} Acc: {train_acc:.3f} | "
+            #       f"Val Loss: {val_loss:.4f} Acc: {val_acc:.3f}")
+
+            if val_loss < best_loss - 1e-4:
+                best_loss = val_loss
+                best_epoch = epoch + 1
+                best_loss_acc = val_acc
+                counter = 0
+                best_loss_kappa = val_kappa
+                print(f"Fold {i + 1}, Epoch {epoch + 1}/{epochs} | "
+                      f"Train Loss: {train_loss:.4f} Acc: {train_acc:.4f} Train Kappa: {train_kappa:.4f} | "
+                      f"Val Loss: {val_loss:.4f} Acc: {val_acc:.4f} Val Kappa: {val_kappa:.4f}")
+            else:
+                counter += 1
+
+            if counter >= patience:
+                print("Early stopping")
+                best_epochs.append(best_epoch)
+                best_losses.append(best_loss)
+                best_loss_accs.append(best_loss_acc)
+                best_losses_kappas.append(best_loss_kappa)
+                break
+
+    # 取 epoch 的中位数作为最终训练 epoch
+    median_epoch = int(np.median(best_epochs))
+    # 计算均值和标准差（loss 和 accuracy）
+    mean_loss = np.mean(best_losses)
+    std_loss = np.std(best_losses)
+    mean_acc = np.mean(best_loss_accs)
+    std_acc = np.std(best_loss_accs)
+    mean_kappa = np.mean(best_losses_kappas)
+    std_kappa = np.std(best_losses_kappas)
+    # 打印结果
+    print(f"Median Epoch for subject{subjectId}: {median_epoch}")
+    print(f"Validation Loss: {mean_loss:.4f} ± {std_loss:.4f}")
+    print(f"Accuracy at Best Loss: {mean_acc:.4f} ± {std_acc:.4f}")
+    print(f"Kappa at Best Loss: {mean_kappa:.4f} ± {std_kappa:.4f}")
+    return median_epoch
+
+
+def train_all_kfold():
+    best_epochs = []
+
+    for i in range(9):
+        epoch = train_kfold(subjectId=i + 1, device=torch.device("cuda"))
+        best_epochs.append(epoch)
+
+    median_epoch = int(np.median(best_epochs))
+    print(f"Median Epoch for final training: {median_epoch}")
---
+++ train_one_epoch.py
@@ -0,0 +1,63 @@
+import torch
+from sklearn.metrics import cohen_kappa_score
+
+
+def train_one_epoch(model, loader, optimizer, criterion, device):
+    model.train()
+    total_loss = 0
+    correct = 0
+    total = 0
+    all_preds = []
+    all_labels = []
+
+    for X, y in loader:
+        X = X.to(device)
+        y = y.to(device)
+
+        optimizer.zero_grad()
+        logits = model(X)  # 输出 shape = (batch, 4)
+
+        loss = criterion(logits, y)
+        loss.backward()
+        optimizer.step()
+
+        total_loss += loss.item()
+        pred = logits.argmax(dim=1)
+        correct += (pred == y).sum().item()
+        total += y.size(0)
+
+        all_preds.append(pred.detach().cpu())
+        all_labels.append(y.detach().cpu())
+
+    all_preds = torch.cat(all_preds).flatten().numpy()
+    all_labels = torch.cat(all_labels).flatten().numpy()
+
+    return total_loss / len(loader), correct / total, cohen_kappa_score(all_preds, all_labels)
+
+
+def eval_one_epoch(model, loader, criterion, device):
+    model.eval()
+    total_loss = 0
+    correct = 0
+    total = 0
+    all_preds = []
+    all_labels = []
+
+    with torch.no_grad():
+        for X, y in loader:
+            X = X.to(device)
+            y = y.to(device)
+
+            logits = model(X)
+            total_loss += criterion(logits, y).item()
+            pred = logits.argmax(dim=1)
+            correct += (pred == y).sum().item()
+            total += y.size(0)
+
+            all_preds.append(pred.detach().cpu())
+            all_labels.append(y.detach().cpu())
+
+    all_preds = torch.cat(all_preds).flatten().numpy()
+    all_labels = torch.cat(all_labels).flatten().numpy()
+
+    return total_loss / len(loader), correct / total, cohen_kappa_score(all_preds, all_labels)
