98e64d402f6ae9a77bb321f7433be431cf861757
Saikumo <saikumo11@gmail.com>
2026-02-12 10:47:30 +0800

    [Update] 20% valid

--- train.py
+++ train.py
@@ -27,7 +27,7 @@ def train(device, batch_size=64, patience=20, epochs=500):
         X_train, X_val, y_train, y_val = train_test_split(
             X_train,
             y_train,
-            test_size=0.1,
+            test_size=0.2,
             random_state=common.random_seed,
             stratify=y_train  # 关键！保持类别比例
         )
