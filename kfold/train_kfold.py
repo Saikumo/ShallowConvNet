d25f5ebc867ee8921b2ba123114678665a66795d
Saikumo <saikumo11@gmail.com>
2026-02-24 15:28:54 +0800

    [Update] update kfold wandb

--- kfold/train_kfold.py
+++ kfold/train_kfold.py
@@ -7,6 +7,8 @@ from torch.utils.data import DataLoader
 import wandb
 import os
 from dataclasses import dataclass
+from datetime import datetime
+from dataclasses import asdict
 
 
 def train_kfold(config):
@@ -16,13 +18,21 @@ def train_kfold(config):
     best_loss_accs = []
     best_losses_kappas = []
 
-    run = wandb.init(
+    timestamp = datetime.now().strftime("%Y%m%d-%H%M%S")
+    summary_run = wandb.init(
         entity="saikumo11-saikumo-s",
         project="ShallowConvNet-KFold",
+        name=f"subject{config.subject_id}_summary_{timestamp}",
         config=config,
     )
 
     for i, fold in enumerate(folds):
+        run = wandb.init(
+            entity="saikumo11-saikumo-s",
+            project="ShallowConvNet-KFold",
+            config={**asdict(config), "fold": i + 1},
+        )
+
         X_train, y_train, X_val, y_val = (fold[k] for k in ['X_train', 'y_train', 'X_val', 'y_val'])
 
         train_dataset = EEGDataset(X_train, y_train)
@@ -48,9 +58,9 @@ def train_kfold(config):
                                                                  config.device)
             val_loss, val_acc, val_kappa = eval_one_epoch(model, val_loader, criterion, config.device)
 
-            run.log({f"train_loss_fold{i}": train_loss, f"train_acc_fold{i}": train_acc,
-                     f"train_kappa_fold{i}": train_kappa, f"val_loss_fold{i}": val_loss, f"val_acc_fold{i}": val_acc,
-                     f"val_kappa_fold{i}": val_kappa}, step=epoch)
+            run.log(
+                {"train_loss_": train_loss, "train_acc_": train_acc, "train_kappa_": train_kappa, "val_loss_": val_loss,
+                 "val_acc_": val_acc, "val_kappa_": val_kappa})
 
             if val_loss < best_loss - 1e-4:
                 best_loss = val_loss
@@ -66,6 +76,7 @@ def train_kfold(config):
                 best_loss_accs.append(best_loss_acc)
                 best_losses_kappas.append(best_loss_kappa)
                 break
+        run.finish()
 
     # 计算均值和标准差（loss 和 accuracy）
     mean_loss = np.mean(best_losses)
@@ -74,9 +85,9 @@ def train_kfold(config):
     std_acc = np.std(best_loss_accs)
     mean_kappa = np.mean(best_losses_kappas)
     std_kappa = np.std(best_losses_kappas)
-    run.log({"mean_loss": mean_loss, "std_loss": std_loss, "mean_acc": mean_acc, "std_acc": std_acc,
-             "mean_kappa": mean_kappa, "std_kappa": std_kappa})
-    run.finish()
+    summary_run.log({"mean_loss": mean_loss, "std_loss": std_loss, "mean_acc": mean_acc, "std_acc": std_acc,
+                     "mean_kappa": mean_kappa, "std_kappa": std_kappa})
+    summary_run.finish()
 
 
 def train_all_kfold():
