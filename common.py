bcd9286d50d118ba6b8a44d47b6c65708823ae0e
Saikumo <saikumo11@gmail.com>
2026-02-25 14:50:52 +0800

    [Update] train with wandb

--- common.py
+++ common.py
@@ -1,5 +1,6 @@
 import random
 from dataclasses import dataclass
+from typing import Optional
 
 import numpy as np
 import torch
@@ -9,15 +10,15 @@ random_seed = 42
 
 @dataclass
 class Config:
-    subject_id: int
-    device: torch.device
-    patience: int
-    epochs: int
-    batch_size: int
-    kfold_n_splits: int
-    lr: float
-    adamw_eps: float
-    weight_decay: float
+    subject_id: Optional[int] = None
+    device: Optional[torch.device] = None
+    patience: Optional[int] = None
+    epochs: Optional[int] = None
+    batch_size: Optional[int] = None
+    kfold_n_splits: Optional[int] = None
+    lr: Optional[float] = None
+    adamw_eps: Optional[float] = None
+    weight_decay: Optional[float] = None
 
 
 def set_seed(seed=random_seed):
