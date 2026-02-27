79c03df3a38b55776d93abb612554aaeeca938ea
Saikumo <saikumo11@gmail.com>
2026-02-27 17:13:57 +0800

    [Create] use moabb benchmark

--- demo/demo.py
+++ demo/demo.py
@@ -3,7 +3,7 @@ from moabb import benchmark
 from moabb.datasets import BNCI2014_001
 
 results = benchmark(
-    pipelines="pipeline.yml",
+    pipelines="./pipeline.yml",
     evaluations=["WithinSession"],
     paradigms=["MotorImagery"],
     include_datasets=[BNCI2014_001()],
