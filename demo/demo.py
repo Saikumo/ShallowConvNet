a05835c363f4920ed40ad6181036b13f00839c99
Saikumo <saikumo11@gmail.com>
2026-02-27 17:09:30 +0800

    [Create] use moabb benchmark

---
+++ demo/demo.py
@@ -0,0 +1,14 @@
+import moabb
+from moabb import benchmark
+from moabb.datasets import BNCI2014_001
+
+results = benchmark(
+    pipelines="/Users/saikumo/Research/Project/ShallowConvNet/demo/pipeline.yml",
+    evaluations=["WithinSession"],
+    paradigms=["MotorImagery"],
+    include_datasets=[BNCI2014_001()],
+    results="./results/",
+    overwrite=False,
+    output="./benchmark/",
+    plot=False,
+)
